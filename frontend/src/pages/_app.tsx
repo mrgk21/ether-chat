4b38fb70cb24d11f346fc545b97b537f4ca5ba0d
Gaurav Kelkar <gauravkelkar20@gmail.com>
2023-03-18 18:40:13 +0530

    build: fixed lint config & added node rc files

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
+const App = ({ Component, pageProps }: AppProps) => {
 	return <Component {...pageProps} />;
-}
+};
+
+export default App;
