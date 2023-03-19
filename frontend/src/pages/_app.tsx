a672846426aa6b15ebb685eb38926244c903731d
Gaurav Kelkar <gauravkelkar20@gmail.com>
2023-03-19 13:23:09 +0530

    dev: added sidebar

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,13 +1,17 @@
 import type { AppProps } from "next/app";
 import Init from "../components/init";
 import Navbar from "../components/navbar";
+import Sidebar from "../components/sidebar";
 import "../global.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return (
 		<Init>
 			<Navbar />
-			<Component {...pageProps} />
+			<div className="flex">
+				<Sidebar groups={null} chats={null} />
+				<Component {...pageProps} />
+			</div>
 		</Init>
 	);
 };
