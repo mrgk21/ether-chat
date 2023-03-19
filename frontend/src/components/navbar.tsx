a69950f532b362cc67d672e3e12aad84938c33fe
Gaurav Kelkar <gauravkelkar20@gmail.com>
2023-03-19 13:47:01 +0530

    fix: removed sidebar, added its contents in nav

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -2,7 +2,14 @@ import { useEffect, useState } from "react";
 import { useAccount, useConnect, useDisconnect, useSignMessage } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
 
-const Navbar = () => {
+type itemType = { label: string; name: string; onClick: (_event: React.MouseEvent<HTMLElement>) => void };
+
+interface SidebarProps {
+	groups: Array<itemType> | null;
+	chats: Array<itemType> | null;
+}
+
+const Navbar = ({ groups, chats }: SidebarProps) => {
 	const { signMessageAsync } = useSignMessage({
 		onError: (error) => console.log("error:", error.message),
 	});
@@ -30,9 +37,31 @@ const Navbar = () => {
 	};
 
 	return (
-		<nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 sticky w-full z-40 top-0">
+		<nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 sticky w-full z-10 top-0">
 			<div className="flex flex-wrap items-center mx-auto ">
-				<div className="flex md:order-2 ml-auto items-center">
+				<div className="flex ml-auto items-center">
+					<ul className="p-2">
+						<h1 className="text-xl text-white">Groups</h1>
+						{groups &&
+							groups.map((group) => (
+								<li className="hover:cursor-pointer p-2 pl-4 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+									<button type="button" name={group.name} onClick={(e) => group.onClick(e)}>
+										{group.label}
+									</button>
+								</li>
+							))}
+					</ul>
+					<ul className="p-2">
+						<h1 className="text-xl text-white">Chats</h1>
+						{chats &&
+							chats.map((chat) => (
+								<li className="hover:cursor-pointer p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+									<button type="button" name={chat.name} onClick={(e) => chat.onClick(e)}>
+										{chat.label}
+									</button>
+								</li>
+							))}
+					</ul>
 					{!_isConnected ? (
 						<button
 							type="button"
@@ -53,29 +82,6 @@ const Navbar = () => {
 							<p className="text-white font-mono ml-2 w-[8rem] truncate">{address}</p>
 						</div>
 					)}
-				</div>
-
-				<div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
-					<ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-						<li>
-							<p className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white">
-								Home
-							</p>
-						</li>
-						<li>
-							<p className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-								About
-							</p>
-						</li>
-						<li>
-							<p className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-								Services
-							</p>
-						</li>
-						<li className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
-							Contact
-						</li>
-					</ul>
 				</div>
 			</div>
 		</nav>
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,17 +1,13 @@
 import type { AppProps } from "next/app";
 import Init from "../components/init";
 import Navbar from "../components/navbar";
-import Sidebar from "../components/sidebar";
 import "../global.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
 	return (
 		<Init>
-			<Navbar />
-			<div className="flex">
-				<Sidebar groups={null} chats={null} />
-				<Component {...pageProps} />
-			</div>
+			<Navbar groups={null} chats={null} />
+			<Component {...pageProps} />
 		</Init>
 	);
 };
